cabb6913d75af200e8d1b5466c22902d6570d720
Guilherme <francaguilherme27@gmail.com>
2023-11-01 19:25:08 -0300

    Ajuste do FAB e Logo

--- app/src/main/java/com/eventclick/faturappmicro/MainActivity.java
+++ app/src/main/java/com/eventclick/faturappmicro/MainActivity.java
@@ -1,6 +1,8 @@
 package com.eventclick.faturappmicro;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
     private SmartTabLayout smartTabLayout;
     private ViewPager viewPager;
+
+    private int currentPosition = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +45,40 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
         smartTabLayout.setViewPager(viewPager);
         viewPager.setCurrentItem(1);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                currentPosition = position;
+                if (position == 0) {
+                    binding.fab.setVisibility(View.VISIBLE);
+                    binding.fab.setImageResource(R.drawable.ic_add_client);
+                } else if (position == 1) {
+                    binding.fab.setVisibility(View.GONE);
+                } else if (position == 2) {
+                    binding.fab.setVisibility(View.VISIBLE);
+                    binding.fab.setImageResource(R.drawable.ic_add_account);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (currentPosition == 0) {
+                    Toast.makeText(getApplicationContext(), "Adiciona cliente", Toast.LENGTH_SHORT).show();
+                } else if (currentPosition == 2) {
+                    Toast.makeText(getApplicationContext(), "Adiciona conta", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
