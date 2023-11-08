cd11c9447091e74e6c26acbe81088256e63c1dba
Guilherme <francaguilherme27@gmail.com>
2023-11-08 19:19:40 -0300

    Ajustes de interface

--- app/src/main/java/com/eventclick/faturappmicro/fragments/AccountsFragment.java
+++ app/src/main/java/com/eventclick/faturappmicro/fragments/AccountsFragment.java
@@ -4,15 +4,18 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
+import android.text.method.DigitsKeyListener;
 import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.content.ContextCompat;
@@ -94,8 +97,23 @@ public class AccountsFragment extends Fragment implements ObserveFragment {
 
         View view = getLayoutInflater().inflate(R.layout.dialog_add_account, null);
 
+        ToggleButton toggleButton = view.findViewById(R.id.toggleButton);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    toggleButton.setBackgroundColor(getContext().getColor(R.color.accent));
+                } else {
+                    toggleButton.setBackgroundColor(getContext().getColor(R.color.redAccent));
+                }
+            }
+        });
+
         Spinner spinner = view.findViewById(R.id.spinnerClient);
         spinner.setAdapter(new AdapterSpinner(getContext(), clients));
+
+        EditText value = view.findViewById(R.id.editTextValue);
+        value.setKeyListener(DigitsKeyListener.getInstance("0123456789,"));
 
         EditText editExpiration = view.findViewById(R.id.editTextExpiration);
         editExpiration.addTextChangedListener(new filterDate(editExpiration));
@@ -125,6 +143,7 @@ public class AccountsFragment extends Fragment implements ObserveFragment {
     }
 
     private Account bind(View view) {
+        ToggleButton toggleButton = view.findViewById(R.id.toggleButton);
         EditText viewDescription = view.findViewById(R.id.editTextDescription);
         EditText viewValue = view.findViewById(R.id.editTextValue);
         Switch viewPaid = view.findViewById(R.id.switchPaid);
@@ -137,7 +156,9 @@ public class AccountsFragment extends Fragment implements ObserveFragment {
         }
 
         String description = viewDescription.getText().toString();
-        double value = Double.parseDouble(viewValue.getText().toString());
+        double value = toggleButton.isChecked() ?
+                Double.parseDouble(viewValue.getText().toString().replace(",", ".")) :
+                Double.parseDouble("-" + viewValue.getText().toString().replace(",", "."));
         boolean paid = viewPaid.isChecked();
         Long clientId = viewClient.getSelectedItemId() == -1L ? null : viewClient.getSelectedItemId();
         java.sql.Date expiration = new Date(new java.util.Date().getTime());
@@ -150,7 +171,14 @@ public class AccountsFragment extends Fragment implements ObserveFragment {
             Log.i("DATA FORMAT", "error at converting date");
         }
 
-        Account account = new Account(clientId, description, value, expiration, paid);
+        Account account;
+
+        if (paid && viewExpiration.getText().toString().isEmpty()){
+            Date paidAt = expiration;
+            account = new Account(clientId, description, value, paidAt, expiration, paid);
+        } else {
+            account = new Account(clientId, description, value, expiration, paid);
+        }
 
         return account;
     }
