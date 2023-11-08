d916525a9856c7b4976af49c8bb65e95176a4aff
Guilherme <francaguilherme27@gmail.com>
2023-11-08 18:03:11 -0300

    Atualização do filtro de data

--- app/src/main/java/com/eventclick/faturappmicro/helpers/filters/filterDate.java
+++ app/src/main/java/com/eventclick/faturappmicro/helpers/filters/filterDate.java
@@ -37,12 +37,40 @@ public class filterDate implements TextWatcher {
                 break;
             }
 
-            char c = mask.charAt(i);
-            if (c == '#') {
-                formattedString += unmask.charAt(index);
+            char cMask = mask.charAt(i);
+            char cUnmask = unmask.charAt(index);
+
+            if (cMask == '#') {
+                if (index == 0 && Integer.parseInt(String.valueOf(cUnmask)) > 3){
+                    formattedString += "0";
+                    unmask = "0" + cUnmask;
+                    i++;
+                    index++;
+                } else if (index == 1 && unmask.charAt(0) == '3' && Integer.parseInt(String.valueOf(cUnmask)) > 1) {
+                    break;
+                } else if (index == 2 && Integer.parseInt(String.valueOf(cUnmask)) > 1) {
+                    formattedString += "0";
+                    unmask = unmask.substring(0, 2);
+                    unmask += "0" + cUnmask;
+                    i++;
+                    index++;
+                } else if (index == 3 && unmask.charAt(2) == '1' && Integer.parseInt(String.valueOf(cUnmask)) > 2) {
+                    break;
+                } else if (index == 4 && Integer.parseInt(String.valueOf(cUnmask)) > 2){
+                    formattedString += "202";
+                    unmask = unmask.substring(0, 4);
+                    unmask += "202" + cUnmask;
+                    i += 3;
+                    index += 3;
+                } else if (index == 5 && unmask.charAt(4) == '1' && Integer.parseInt(String.valueOf(cUnmask)) < 9) {
+                    break;
+                } else if (index == 5 && unmask.charAt(4) == '2' && Integer.parseInt(String.valueOf(cUnmask)) > 0) {
+                    break;
+                }
+                formattedString += cUnmask;
                 index++;
             } else {
-                formattedString += c;
+                formattedString += cMask;
             }
         }
 
