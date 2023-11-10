9a49b0ca207e89a69953fefe3b30af37f5143488
Guilherme <francaguilherme27@gmail.com>
2023-11-10 17:51:59 -0300

    Documentação e refatoração

--- app/src/main/java/com/eventclick/faturappmicro/helpers/dbHelpers/models/Client.java
+++ app/src/main/java/com/eventclick/faturappmicro/helpers/dbHelpers/models/Client.java
@@ -1,5 +1,8 @@
 package com.eventclick.faturappmicro.helpers.dbHelpers.models;
 
+/**
+ * Modelo de dados para representação do cliente
+ */
 public class Client {
     private Long id;
     private String name;
@@ -8,6 +11,16 @@ public class Client {
     private String address;
     private String pix;
 
+    /**
+     * Construtor da classe com todos os atributos
+     *
+     * @param id Identificador do cliente
+     * @param name Nome do cliente
+     * @param cpf_cnpj CPF ou CNPJ do cliente
+     * @param contact Contato do cliente
+     * @param address Endereço do cliente
+     * @param pix Chave PIX do cliente
+     */
     public Client(Long id, String name, String cpf_cnpj, String contact, String address, String pix) {
         this.id = id;
         this.name = name;
@@ -17,6 +30,15 @@ public class Client {
         this.pix = pix;
     }
 
+    /**
+     * Contrutor da classe sem o identificador do cliente
+     *
+     * @param name Nome do cliente
+     * @param cpf_cnpj CPF ou CNPJ do cliente
+     * @param contact Contato do cliente
+     * @param address Endereço do cliente
+     * @param pix Chave PIX do cliente
+     */
     public Client(String name, String cpf_cnpj, String contact, String address, String pix) {
         this.name = name;
         this.cpf_cnpj = cpf_cnpj;
@@ -25,6 +47,12 @@ public class Client {
         this.pix = pix;
     }
 
+    /**
+     * Contrutor da classe para uma instância somente com id e nome do cliente
+     *
+     * @param id Identificador do cliente
+     * @param name Nome do cliente
+     */
     public Client(Long id, String name) {
         this.id = id;
         this.name = name;
@@ -50,31 +78,15 @@ public class Client {
         return cpf_cnpj;
     }
 
-    public void setCpf_cnpj(String cpf_cnpj) {
-        this.cpf_cnpj = cpf_cnpj;
-    }
-
     public String getContact() {
         return contact;
-    }
-
-    public void setContact(String contact) {
-        this.contact = contact;
     }
 
     public String getAddress() {
         return address;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public String getPix() {
         return pix;
-    }
-
-    public void setPix(String pix) {
-        this.pix = pix;
     }
 }
