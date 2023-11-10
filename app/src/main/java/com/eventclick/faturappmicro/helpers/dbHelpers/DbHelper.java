5227ce7a4cd49b92c21e3a4eb6aa42d94db0c20d
Guilherme <francaguilherme27@gmail.com>
2023-11-10 17:32:49 -0300

    Classe não utilizada

--- app/src/main/java/com/eventclick/faturappmicro/helpers/dbHelpers/DbHelper.java
+++ app/src/main/java/com/eventclick/faturappmicro/helpers/dbHelpers/DbHelper.java
@@ -5,18 +5,33 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+/**
+ * Classe para inicializar e gerenciar o banco de dados SQLite
+ * Estende SQLiteOpenHelper
+ */
 public class DbHelper extends SQLiteOpenHelper {
     public static int VERSION = 1;
     public static final String NAME_DB = "DB_FINANCE";
     public static final String CLIENT_TABLE = "clients";
     public static final String ACCOUNTS_TABLE = "accounts";
 
+    /**
+     * Construtor da classe
+     *
+     * @param context Contexto da aplicação
+     */
     public DbHelper(Context context) {
         super(context, NAME_DB, null, VERSION);
     }
 
+    /**
+     * Chamado quando o banco de dados é utilizado pela primeira vez
+     *
+     * @param sqLiteDatabase Instância do banco de dados
+     */
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        // Estrutura da tabela 'clients'
         String sqlClient = String.format(
                 "CREATE TABLE IF NOT EXISTS %s (" +
                         "id INTEGER PRIMARY KEY AUTOINCREMENT," +
@@ -27,6 +42,7 @@ public class DbHelper extends SQLiteOpenHelper {
                         "pix VARCHAR(180)" +
                 ");", CLIENT_TABLE);
 
+        // Estrutura da tabela 'accounts'
         String sqlAccount = String.format(
                 "CREATE TABLE IF NOT EXISTS %s (" +
                         "id INTEGER PRIMARY KEY AUTOINCREMENT," +
@@ -39,6 +55,7 @@ public class DbHelper extends SQLiteOpenHelper {
                         "FOREIGN KEY (client_id) REFERENCES %s(id)" +
                 ");", ACCOUNTS_TABLE, CLIENT_TABLE);
         try {
+            // Executa os comendos SQL para criação das tabelas
             sqLiteDatabase.execSQL(sqlClient);
             sqLiteDatabase.execSQL(sqlAccount);
             Log.i("INFO DB", "successful database creation");
@@ -47,8 +64,11 @@ public class DbHelper extends SQLiteOpenHelper {
         }
     }
 
+    /**
+     * Método utilizado para atualizar o banco de dados
+     * Não se aplica nesse contexto
+     */
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
     }
 }
