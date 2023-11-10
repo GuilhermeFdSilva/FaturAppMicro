ca47e8032840402b5de13f01b1f8ff906219b43e
Guilherme <francaguilherme27@gmail.com>
2023-11-10 19:49:24 -0300

    Documentação e refatoração

--- app/src/main/java/com/eventclick/faturappmicro/fragments/adapters/AdapterSpinner.java
+++ app/src/main/java/com/eventclick/faturappmicro/fragments/adapters/AdapterSpinner.java
@@ -12,30 +12,64 @@ import com.eventclick.faturappmicro.helpers.dbHelpers.models.Client;
 
 import java.util.List;
 
+/**
+ * Adaptador personalizado para preencher o Spinner de clientes
+ */
 public class AdapterSpinner extends BaseAdapter {
-    private Context context;
-    private List<Client> clients;
+    private final Context context;
+    private final List<Client> clients;
 
+    /**
+     * Construtor da classe
+     *
+     * @param context Contexto da aplicação
+     * @param clients Lista de clientes para o Spinner
+     */
     public AdapterSpinner(Context context, List<Client> clients) {
         this.context = context;
         this.clients = clients;
     }
 
+    /**
+     * Obtém o número de itens
+     *
+     * @return Número de clientes
+     */
     @Override
     public int getCount() {
         return clients.size();
     }
 
+    /**
+     * Obtém um item específico
+     *
+     * @param i Posição do item
+     * @return Cliente na posição i
+     */
     @Override
     public Object getItem(int i) {
         return clients.get(i);
     }
 
+    /**
+     * Obtém o ID do item
+     *
+     * @param i Posição do item
+     * @return ID do cliente na posição i
+     */
     @Override
     public long getItemId(int i) {
         return clients.get(i).getId();
     }
 
+    /**
+     * Obtém a View que será exibida no Spinner
+     *
+     * @param i Posição do item na lista
+     * @param view A View utilizada
+     * @param viewGroup O ViewGroup pai
+     * @return View formatada para exibição
+     */
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
