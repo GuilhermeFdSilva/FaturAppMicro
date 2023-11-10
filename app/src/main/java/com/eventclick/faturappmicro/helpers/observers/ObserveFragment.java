3598aa2addf41880bb05b90019fdc1765ffa18b4
Guilherme <francaguilherme27@gmail.com>
2023-11-10 16:27:43 -0300

    Documentação e refatoração

--- app/src/main/java/com/eventclick/faturappmicro/helpers/observers/ObserveFragment.java
+++ app/src/main/java/com/eventclick/faturappmicro/helpers/observers/ObserveFragment.java
@@ -1,5 +1,11 @@
 package com.eventclick.faturappmicro.helpers.observers;
 
+/**
+ * Interface para os fragments atuarem como observadores
+ */
 public interface ObserveFragment {
+    /**
+     * Método para atualização dos dados dos fragments
+     */
     void update();
 }
