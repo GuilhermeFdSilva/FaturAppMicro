35c39b6f80156e83e3c373973e7dfe8e8a8ee498
Guilherme <francaguilherme27@gmail.com>
2023-11-10 18:04:54 -0300

    Documentação e refatoração

--- app/src/main/java/com/eventclick/faturappmicro/helpers/dbHelpers/DAO/IDAO.java
+++ app/src/main/java/com/eventclick/faturappmicro/helpers/dbHelpers/DAO/IDAO.java
@@ -2,10 +2,15 @@ package com.eventclick.faturappmicro.helpers.dbHelpers.DAO;
 
 import java.util.List;
 
+/**
+ * Interface para um Data Access Object (DAO) genérico
+ *
+ * @param <T> Modelo de objeto a ser utilizado
+ */
 public interface IDAO<T> {
-    public boolean save(T objeto);
-    public boolean update(T objeto);
-    public boolean delete(T objeto);
-    public T getById(int idItem);
-    public List<T> list();
+    boolean save(T objeto);
+    boolean update(T objeto);
+    boolean delete(T objeto);
+    T getById(int idItem);
+    List<T> list();
 }
